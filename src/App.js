3514dbcc4326e7e119842618f43a73cd4a66a1d1
Tshering <engineertshering@gmail.com>
2021-03-04 09:46:10 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {Cardlist} from './components/card-list/card-list.component';
+import {Searchbox} from './components/search-box/search-box.component';
+
+import './App.css';
+
+
+class App extends Component{
+       constructor(){
+            super();
+            this.state = { 
+                          monsters: [],
+                          searchField: ''
+            }
+       }
+       componentDidMount(){
+                 fetch('https://jsonplaceholder.typicode.com/users')
+                 .then(response => response.json())
+                 .then(users => this.setState({monsters: users}))
+       }
+       handleChange = (e) => {
+                          this.setState({searchField: e.target.value})
+       }
+       render(){
+                  const { monsters, searchField } = this.state;
+                  const filteredMonsters = monsters.filter((monster) => monster.name.includes(searchField))
+        return (
+          <div className="App">
+          <h1>Welcome to Monster List</h1>
+          <Searchbox placeholder = 'search monsters' handleChange={this.handleChange} />
+          <Cardlist monsters = {filteredMonsters} />
+          </div>
+
+        );       
+       }
+}
+
+
+export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './search-box.styles.css';
+
+export const Searchbox = ({placeholder, handleChange}) => (
+         <input className="search" 
+                type="search" 
+                placeholder={placeholder}
+                onChange={handleChange} />
+)
